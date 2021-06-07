d071be4b44bb4ef93cebd77fe3a1c77849728d98
Nurudeen Amedu <nurudeen.amedu@gmail.com>
2021-06-07 18:37:27 +0100

    adding the task

---
+++ src/components/Step1.jsx
@@ -0,0 +1,10 @@
+export default function Step1({ form, updateForm }){
+
+    return(
+        <div className='onboard-form'>
+            <h3>Let's set up your account</h3>
+            <div className='small-p flex-row'>
+                <p>Already have an account? </p><a href='/'>Sign in</a>
+            </div>
+        </div>);
+}
---
+++ src/pages/Onboarding.jsx
@@ -0,0 +1,39 @@
+import { useState } from 'react'
+import { GiPlainCircle, GiCircle } from "react-icons/gi";
+
+import Step1 from '../components/Step1'
+
+export default function Onboarding(){
+    const [ form, setForm ] = useState({})
+
+    const updateForm = (e) => {
+        setForm({
+            ...form,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    return(<div className='flex-row onboard-container'>
+        <div className='flex-column'>
+            <div className='flex-row steps'>
+                <p>Step 1 of 3</p>
+                <GiPlainCircle />
+                <div className="small">
+                    <GiCircle />
+                </div>
+                <div className="small">
+                    <GiCircle />
+                </div>
+            </div>
+            <Step1 
+                form={form}
+                updateForm={updateForm}
+            />
+        </div>
+        <div className='cta-area'>
+            <h2>Dummy Heading</h2>
+            <h5>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris hendrerit justo ut ultricies malesuada. Etiam tincidunt tempor lacus quis congue. Aliqua</h5>
+        </div>
+    </div>);
+
+}
